cae49a58a1ba042c0a452f432f6dadf904a4bd80
n3-rd <godwinjdayo@gmail.com>
2024-12-03 14:47:08 +0100

    added active link background

--- src/lib/components/nav-main.svelte
+++ src/lib/components/nav-main.svelte
@@ -3,16 +3,16 @@
 	import * as Sidebar from "$lib/components/ui/sidebar/index.js";
 	import { useSidebar } from "$lib/components/ui/sidebar/index.js";
 	import { Home, Users, ChartColumnBig, Smartphone, BookOpen, Settings } from "lucide-svelte";
+	import { page } from "$app/stores";
 
-	
-	let items = [
+	let items = $state([
 		{ title: "Inbox", url: "/inbox", icon: Home, href: "/" },
 		{ title: "Contacts", url: "/contacts", icon: Users, href: "/contacts" },
 		{ title: "Reports", url: "/reports", icon: ChartColumnBig, href: "/reports" },
 		{ title: "Leadbox", url: "/leadbox", icon: Smartphone, href: "/leadbox" },
 		{ title: "Leadform", url: "/leadform", icon: BookOpen, href: "/leadform" },
 		{ title: "Settings", url: "/settings", icon: Settings, href: "/settings" },
-	];
+	]);
 
 	const sidebar = useSidebar();
 </script>
@@ -21,10 +21,15 @@
 	<Sidebar.Menu>
 		{#each items as mainItem (mainItem.title)}
 			<Sidebar.MenuItem class="gap-4 !text-5xl">
-				<a href={mainItem.href}>
-					<Sidebar.MenuButton class="!py-7 flex items-center gap-3 pl-9 hover:text-primary font-medium">
+				<a 
+					href={mainItem.href}
+					class="w-full"
+				>
+					<Sidebar.MenuButton 
+						class="!py-7 flex items-center gap-3 pl-9 hover:bg-white font-medium w-full {$page.url.pathname === mainItem.href ? 'bg-white text-primary' : ''}"
+					>
 						<mainItem.icon class="!w-6 !h-6" />
-					{mainItem.title}
+						{mainItem.title}
 					</Sidebar.MenuButton>
 				</a>
 			</Sidebar.MenuItem>
